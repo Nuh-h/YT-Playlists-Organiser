a7d2d3b8c2d47b1cc840ce36a75a662f935b6c2a
Nuh-remote <69435206+Nuh-h@users.noreply.github.com>
2021-04-02 00:26:24 +0100

    prepare for production

--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -21,6 +21,10 @@ const config = {
             {
                 test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
                 use: 'file-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader']
             }
         ]
     }
